fb5d742b8a1a95af78af83aa6951036dd1c3da9c
ashish <ashishmangla1991@gmail.com>
2020-05-09 16:14:06 +0530

    add new file

---
+++ index.js
@@ -0,0 +1,33 @@
+//process.stdout.write(Math.random().toString());
+//console.log(Math.random());
+//console.log(process)
+//console.dir(process,{depth:1})
+
+
+// function x(){
+//     console.log(arguments);
+// }
+// x(1,2,3)
+
+
+//console.log(arguments);
+
+// console.log('In index2.js')
+// exports.answer = 42;
+// module.exports.answer2 = 15 // alias of exports
+// console.log(exports);
+
+
+//(function(exports,require,module,__filename,__dirname))
+
+// module.exports = () =>{
+//     console.log("Hello");
+// }
+module.exports = {
+    f1:() =>{
+        console.log("Hello");
+    },
+    f2:() =>{
+        console.log("Hello mangla");
+    }
+}
